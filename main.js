ffc3c100731eeea37596ffa0d7a60fe8ef50cd4a
Danny Le <hoangsonle2711@gmail.com>
2022-01-20 23:53:38 -0800

    add comment

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./lavaB.jpg");
 ASSET_MANAGER.queueDownload("./lavafish.png");
 
-ASSET_MANAGER.downloadAll(() => {
+ASSET_MANAGER.downloadAll(() => { 
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	//addbackground
 	gameEngine.addEntity(new lavafish(gameEngine));
 	gameEngine.addEntity(new background());
 	
