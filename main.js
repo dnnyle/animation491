bdf1a6488ca334431b68cf5519235451e279e78f
Danny Le <hoangsonle2711@gmail.com>
2022-01-20 23:08:41 -0800

    lavafish without background

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
-
+ASSET_MANAGER.queueDownload("./lavafish.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
+	gameEngine.addEntity(new lavafish(gameEngine));
 	gameEngine.init(ctx);
 
 	gameEngine.start();
